2774cef03f26ebb7e99ee00a637f2d3543eaa489
Juan <Juan@DESKTOP-NSNEPC8>
2017-05-29 22:29:24 +0200

    url connect

---
+++ src/DAO/connectURL.java
@@ -0,0 +1,43 @@
+package DAO;
+import java.sql.*;  
+
+public class connectURL {  
+
+   public static void main(String[] args) {  
+
+      // Create a variable for the connection string.  
+      String connectionUrl = "jdbc:sqlserver://DESKTOP-NSNEPC8\\SQLJUAN:1433;" +  
+         "databaseName=AdventureWorks;user=cosmopolitans;password=fresas";  
+
+      // Declare the JDBC objects.  
+      Connection con = null;  
+      Statement stmt = null;  
+      ResultSet rs = null;  
+
+      try {  
+         // Establish the connection.  
+         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  
+         con = DriverManager.getConnection(connectionUrl);  
+
+         // Create and execute an SQL statement that returns some data.  
+         String SQL = "SELECT TOP 10 * FROM Empleado";  
+         stmt = con.createStatement();  
+         rs = stmt.executeQuery(SQL);  
+
+         // Iterate through the data in the result set and display it.  
+         while (rs.next()) {  
+            System.out.println(rs.getString(4) + " " + rs.getString(6));  
+         }  
+      }  
+
+      // Handle any errors that may have occurred.  
+      catch (Exception e) {  
+         e.printStackTrace();  
+      }  
+      finally {  
+         if (rs != null) try { rs.close(); } catch(Exception e) {}  
+         if (stmt != null) try { stmt.close(); } catch(Exception e) {}  
+         if (con != null) try { con.close(); } catch(Exception e) {}  
+      }  
+   }  
+}  
