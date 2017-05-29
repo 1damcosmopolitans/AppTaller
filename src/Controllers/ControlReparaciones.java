3c01e1de35aedac15c12e6ce858eb88b80e7a081
admincenec <admincenec@AULA526.cenecmalaga.net>
2017-05-29 13:25:37 +0200

    control reparaciones

--- src/Controllers/ControlReparaciones.java
+++ src/Controllers/ControlReparaciones.java
@@ -8,8 +8,7 @@ import Models.Vehiculo;
 
 public class ControlReparaciones {
 
-	private static ArrayList<Reparacion> listaReparaciones = new ArrayList<Reparacion>();
-	private static ArrayList<Reparacion> listaReparacionesPagadas = new ArrayList<Reparacion>();  //SE INSERTA EN ESTA LISTA SOLO SI SE INTRODUCE LA FECHA DE ENTREGA, ES DECIR QUE SE HA PAGADO LA REPARACIÓN :)
+	private static ArrayList<Reparacion> listaReparaciones = new ArrayList<Reparacion>();//SE INSERTA EN ESTA LISTA SOLO SI SE INTRODUCE LA FECHA DE ENTREGA, ES DECIR QUE SE HA PAGADO LA REPARACIÓN :)
 	
 	public static boolean Buscar(String idRep){
 		Reparacion rep = Obtener(idRep);
@@ -38,6 +37,39 @@ public class ControlReparaciones {
 		return rep;
 	}
 	
+	public ArrayList<Reparacion> listaReparados(String matricula){
+		ArrayList<Reparacion> aux = new ArrayList<Reparacion>();
+		for(Reparacion i: listaTotal(matricula)){
+			if(i.getEstadoAveria().equals("Reparado")){
+				aux.add(i);
+			}
+		}
+		
+		return aux;
+	}
+	
+	public ArrayList<Reparacion> listaAverias(String matricula){
+		ArrayList<Reparacion> aux = new ArrayList<Reparacion>();
+		for(Reparacion i: listaTotal(matricula)){
+			if(!i.getEstadoAveria().equals("Reparado")){
+				aux.add(i);
+			}
+		}
+		
+		return aux;
+	}
+	
+	public ArrayList<Reparacion> listaTotal(String matricula){
+		ArrayList<Reparacion> aux = new ArrayList<Reparacion>();
+		for(Reparacion i: listaReparaciones){
+			if(i.getMatriculaRep().equals(matricula)){
+				aux.add(i);
+			}
+		}
+		
+		return aux;
+	}
+	
 	//IMPORTANTE RESPETAR EL ORDEN DE LAS INSTRUCCIONES YA QUE SI ELIMINAS DE LA LISTA ANTES DE EJECUTAR LA FUNCION ANIADIR
 	//EN EL CASO DE QUE UN USUARIO INTENTE MODIFICAR UNA ID DE UNA REPARACIÓN POR OTRA NUEVA YA EXISTENTE DENTRO DE LA LISTA
 	//SE BORRARÍA LA ANTERIOR Y SE SUSTITUIRÍA POR LS NUEVA
@@ -53,41 +85,4 @@ public class ControlReparaciones {
 	 * 2 CASO : SE INGRESA LA FECHA DE SALIDA Y SE ALMACENA LA FICHA DE REPARACION EN LA SEGUNDA LISTA --LAS REPARACIONES PAGADAS
 	 */
 	
-	public static boolean Buscar2(String idRep){
-		Reparacion rep = Obtener2(idRep);
-		
-		if(rep == null){
-			return false;
-		}else{
-			return true;
-		}
-	}
-	
-	public static void Aniadir2(String idRep, String matriculaRep, String fechaIni, String fechaFin,  String averia, String estadoAveria){
-		if(Buscar2(idRep)) throw new RuntimeException("No se puede añadir la reparación porque ya hay una con el mismo identificativo");
-		
-		listaReparacionesPagadas.add(new Reparacion(idRep, matriculaRep, fechaIni, fechaFin, averia, estadoAveria));
-	}	
-	public static Reparacion Obtener2(String idRep){
-		Reparacion rep = null;
-		
-		for(Reparacion i: listaReparacionesPagadas){
-			if(i.getIdRep()== idRep){
-				rep = i;
-			}
-		}
-		
-		return rep;
-	}
-	
-	//IMPORTANTE RESPETAR EL ORDEN DE LAS INSTRUCCIONES YA QUE SI ELIMINAS DE LA LISTA ANTES DE EJECUTAR LA FUNCION ANIADIR
-	//EN EL CASO DE QUE UN USUARIO INTENTE MODIFICAR UNA ID DE UNA REPARACIÓN POR OTRA NUEVA YA EXISTENTE DENTRO DE LA LISTA
-	//SE BORRARÍA LA ANTERIOR Y SE SUSTITUIRÍA POR LS NUEVA
-	
-	public static void Editar2(String idRep, String matriculaRep, String fechaIni, String fechaFin, String averia, String estadoAveria){
-		
-		if(!Buscar2(idRep)) throw new RuntimeException("No se encontro la reparación a editar");
-		Aniadir2(idRep, matriculaRep, fechaIni, fechaFin, averia, estadoAveria);
-		listaReparacionesPagadas.remove(Obtener2(idRep));
-	}
 }
